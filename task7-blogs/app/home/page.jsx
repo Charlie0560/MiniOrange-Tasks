c4221f3030388c1e051e69f3553396d1539d2994
Chaitanya Lokhande <84211348+Charlie0560@users.noreply.github.com>
2024-01-31 17:46:48 +0530

    SSG getStaticProps issue

--- task7-blogs/app/home/page.jsx
+++ task7-blogs/app/home/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+// _app.js
 import React, { useEffect, useState } from 'react'
 const axios = require('axios');
 // import axios from "axios"
@@ -7,7 +8,11 @@ import Cards from "../cards/page"
 import "./home.css"
 
 
-function home() {
+
+
+
+function home({ stockData }) {
+  console.log(stockData)
   const [data, setData] = useState([]);
   // const[coutn,setCount]= useState(0);
 
@@ -32,59 +37,88 @@ function home() {
 
 
 
-  const options = {
-    method: 'GET',
-    url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data',
-    params: {
-      symbol: 'AMRN',
-      region: 'US'
-    },
-    headers: {
-      'X-RapidAPI-Key': '62c9e1f1f5msha037b886bcca680p1d2fe3jsna1be07a3121e',
-      'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
-    }
-  };
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data',
+  //   params: {
+  //     symbol: 'AMRN',
+  //     region: 'US'
+  //   },
+  //   headers: {
+  //     'X-RapidAPI-Key': '62c9e1f1f5msha037b886bcca680p1d2fe3jsna1be07a3121e',
+  //     'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
+  //   }
+  // };
 
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const symbols = ['AMRN', 'AAPL', 'GOOGL'];
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const symbols = ['AMRN', 'AAPL', 'GOOGL'];
 
-      try {
-        const stockData = await Promise.all(
-          symbols.map(async (symbol) => {
-            const stockOptions = { ...options, params: { symbol, region: 'US' } };
-            const response = await axios.request(stockOptions);
-            const slicedData = response.data.prices.slice(0, 1);
-            return slicedData.map((item) => ({ ...item, symbol })); // Add symbol to each data point
-          })
-        );
+  //     try {
+  //       const stockData = await Promise.all(
+  //         symbols.map(async (symbol) => {
+  //           const stockOptions = { ...options, params: { symbol, region: 'US' } };
+  //           const response = await axios.request(stockOptions);
+  //           const slicedData = response.data.prices.slice(0, 1);
+  //           return slicedData.map((item) => ({ ...item, symbol })); // Add symbol to each data point
+  //         })
+  //       );
 
-        setData(stockData.flat());
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  //       setData(stockData.flat());
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
 
-    fetchData();
-  }, [options]);
+  //   fetchData();
+  // }, [options]);
 
-  console.log(data)
+  // console.log(data)
 
   return (<>
     <h1>Stock Details</h1>
     <div className='cards '>
-
-
-      {data.length > 0 && data.map((d) => (
-
-        <Cards options={options} data={d} />
-      ))}
-
-
-    </div>
+        {stockData &&  stockData.length > 0 &&
+          stockData.map((d, index) => <Cards data={d} />)}
+      </div>
+      {/* <Component {...pageProps} /> */}
   </>
   )
+}
+
+
+export async function getStaticProps() {
+  const symbols = ['AMRN', 'AAPL', 'GOOGL'];
+  const options = {
+    method: 'GET',
+    url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data',
+    headers: {
+      'X-RapidAPI-Key': '62c9e1f1f5msha037b886bcca680p1d2fe3jsna1be07a3121e',
+      'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+    },
+  };
+
+  try {
+    console.log("hellow world")
+    const stockData = await Promise.all(
+      symbols.map(async (symbol) => {
+        const stockOptions = { ...options, params: { symbol, region: 'US' } };
+        const response = await axios.request(stockOptions);
+        const slicedData = response.data.prices.slice(0, 1);
+        return slicedData.map((item) => ({ ...item, symbol }));
+      })
+    );
+    console.log(stockData.flat())
+    return {
+      props: {
+        stockData: stockData.flat(),
+      },
+    };
+  } catch (err) {
+    console.error(err);
+    
+  }
 }
 
 export default home
---
+++ task7/src/pages/home/page.jsx
@@ -0,0 +1,64 @@
+"use client";
+// _app.js
+import React, { useEffect, useState } from 'react'
+const axios = require('axios');
+// import axios from "axios"
+// var fetch = require('fetch');
+import Cards from "../cards/page"
+import "./home.css"
+
+
+
+
+
+function home({ stockData }) {
+  console.log(stockData)
+  const [data, setData] = useState([]);
+
+
+  return (<>
+    <h1>Stock Details</h1>
+    <div className='cards '>
+        {stockData &&  stockData.length > 0 &&
+          stockData.map((d, index) => <Cards data={d} />)}
+      </div>
+      {/* <Component {...pageProps} /> */}
+  </>
+  )
+}
+
+
+export async function getStaticProps() {
+  const symbols = ['AMRN', 'AAPL', 'GOOGL'];
+  const options = {
+    method: 'GET',
+    url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data',
+    headers: {
+      'X-RapidAPI-Key': '62c9e1f1f5msha037b886bcca680p1d2fe3jsna1be07a3121e',
+      'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+    },
+  };
+
+  try {
+    console.log("hellow world")
+    const stockData = await Promise.all(
+      symbols.map(async (symbol) => {
+        const stockOptions = { ...options, params: { symbol, region: 'US' } };
+        const response = await axios.request(stockOptions);
+        const slicedData = response.data.prices.slice(0, 1);
+        return slicedData.map((item) => ({ ...item, symbol }));
+      })
+    );
+    console.log(stockData.flat())
+    return {
+      props: {
+        stockData: stockData.flat(),
+      },
+    };
+  } catch (err) {
+    console.error(err);
+    
+  }
+}
+
+export default home
