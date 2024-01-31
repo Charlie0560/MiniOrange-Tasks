f7f9af743f81907a25e39372a5bfc7999b3450b5
Chaitanya Lokhande <84211348+Charlie0560@users.noreply.github.com>
2024-01-31 13:19:44 +0530

    api calling

---
+++ task7-blogs/app/cards/page.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./card.css"
+
+function cards({options, data}) {
+  let symbol=data.symbol;
+
+  let close = data.close;
+  let open = data.open;
+  let high = data.high;
+  let low = data.low;
+  return (
+    <div className='container p-5 carddiv'>
+      <b>STOCK :</b> {symbol}
+      <br />  
+      <b>CLOSE :</b> {close.toFixed(4)}
+      <br />  
+      <b>OPEN :</b>{open.toFixed(4)}
+      <br />  
+      <b>HIGH :</b>{high.toFixed(4)}
+      <br />  
+      <b>LOW :</b>{low.toFixed(4)}
+
+    </div>
+  )
+}
+
+export default cards
---
+++ task7-blogs/app/home/page.jsx
@@ -0,0 +1,90 @@
+"use client";
+import React, { useEffect, useState } from 'react'
+const axios = require('axios');
+// import axios from "axios"
+// var fetch = require('fetch');
+import Cards from "../cards/page"
+import "./home.css"
+
+
+function home() {
+  const [data, setData] = useState([]);
+  // const[coutn,setCount]= useState(0);
+
+  // useEffect(()=>{
+  //   const fetchData = async()=>{
+  //     try{
+
+  //       const response = await fetch('https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=5SQBK6690IA853K2')
+  //       const data =response.json();
+  //       console.log(data)
+
+  //       const openPrice = firstDateData['1. open'];
+  //       console.log(openPrice);
+  //     }catch(err){
+  //       console.log(err);
+  //     }
+
+  //   }
+  //   fetchData();
+  // },[])
+
+
+
+
+  const options = {
+    method: 'GET',
+    url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data',
+    params: {
+      symbol: 'AMRN',
+      region: 'US'
+    },
+    headers: {
+      'X-RapidAPI-Key': '62c9e1f1f5msha037b886bcca680p1d2fe3jsna1be07a3121e',
+      'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
+    }
+  };
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const symbols = ['AMRN', 'AAPL', 'GOOGL'];
+
+      try {
+        const stockData = await Promise.all(
+          symbols.map(async (symbol) => {
+            const stockOptions = { ...options, params: { symbol, region: 'US' } };
+            const response = await axios.request(stockOptions);
+            const slicedData = response.data.prices.slice(0, 1);
+            return slicedData.map((item) => ({ ...item, symbol })); // Add symbol to each data point
+          })
+        );
+
+        setData(stockData.flat());
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [options]);
+
+  console.log(data)
+
+  return (<>
+    <h1>Stock Details</h1>
+    <div className='cards '>
+
+
+      {data.length > 0 && data.map((d) => (
+
+        <Cards options={options} data={d} />
+      ))}
+
+
+    </div>
+  </>
+  )
+}
+
+export default home
